81590be2ec523651adbbb3a8272f690bb200979a
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-09-18 13:20:52 +0530

    added tree background

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+// import rive from "@rive-app/canvas";
+
+const height = (document.body.clientHeight * 3) / 4;
+
+window.addEventListener("scroll", () => {
+  const scollPercentage = (window.scrollY / height) * 100;
+  const scrollPercentageRounded = Math.round(scollPercentage);
+});
+
+const r = new rive.Rive({
+  src: "tree.riv",
+  canvas: document.getElementById("rive"),
+  onLoad: () => {
+    r.resizeDrawingSurfaceToCanvas();
+  },
+});
